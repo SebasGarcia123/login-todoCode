7629c438e89ba821dca37c58be14529da7ed8df8
SebasGarcia123 <sebastiandariogarcia123@gmail.com>
2025-01-06 21:10:08 -0300

    Se crea clases  usuario

---
+++ src/main/java/prog2/login/logica/Usuario.java
@@ -0,0 +1,42 @@
+package prog2.login.logica;
+
+public class Usuario {
+    
+    private int id;
+    private String nombreUsuario;
+    private String contrasenia;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombreUsuario, String contrasenia) {
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
+    
+}
